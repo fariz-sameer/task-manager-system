2aff2b93ad513402e5c1bbf402a24c89fe391cfd
fariz <farizsameer@gmail.com>
2026-07-22 13:02:42 +0300

    updated static files directory settingsl

--- TaskManagementSystem/settings.py
+++ TaskManagementSystem/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "TMS/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "TMS" / "static",
+]
 
 LOGIN_URL = "login"
 
